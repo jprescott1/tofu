a14290a758ac72ecb5037a1192a3a528b41f9701
Jimmy Prescott <bunches_stands_0s@icloud.com>
2024-06-02 22:23:21 -0500

    LAB-3: Build a test VM

--- k8s-lab-proxmox/main.tf
+++ k8s-lab-proxmox/main.tf
@@ -1,4 +1,15 @@
 module "lab" {
   source = "../modules/k8s-lab"
-  
+}
+
+resource "proxmox_vm_qemu" "lab" {
+  name        = "test-vm"
+  target_node = "kube"
+  vmid        = 2000
+  clone       = "ubuntu-2204-template"
+  full_clone  = true
+  memory      = 8000
+  cores       = 2
+  ssh_user    = "jimmy"
+  sshkeys     = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAsycFZCGi6778LPkAq2I9RJlmkNrMwEEiZvGwWp5tvg jimmyjorts@gloogleegloo.com"
 }
