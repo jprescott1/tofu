dd9d88c34015e91413dba10473ead93fcc709ef7
Jimmy Prescott <bunches_stands_0s@icloud.com>
2024-06-05 17:56:39 -0500

    functioning tf

--- k8s-lab-proxmox/main.tf
+++ k8s-lab-proxmox/main.tf
@@ -17,45 +17,6 @@ provider "proxmox" {
   }
 }
 
-resource "proxmox_virtual_environment_vm" "ubuntu_vm" {
-  name      = "test-ubuntu"
-  node_name = "kube"
-  vm_id     = 1000
-
-  agent {
-    enabled = true
-  }
-
-  cpu {
-    cores = 2
-  }
-
-  memory {
-    dedicated = 8000
-  }
-
-  efi_disk {
-    datastore_id = "local-lvm"
-    file_format  = "raw"
-    type         = "4m"
-  }
-
-  disk {
-    datastore_id = "local-lvm"
-    file_id      = proxmox_virtual_environment_file.ubuntu_cloud_image.id
-    interface    = "virtio0"
-    iothread     = true
-    discard      = "on"
-    size         = 40
-  }
-
-  network_device {
-    bridge = "vmbr0"
-  }
-
-  started = false
-}
-
 resource "proxmox_virtual_environment_file" "ubuntu_cloud_image" {
   content_type = "iso"
   datastore_id = "local"
@@ -66,55 +27,28 @@ resource "proxmox_virtual_environment_file" "ubuntu_cloud_image" {
   }
 }
 
-data "local_file" "ssh_public_key" {
-  filename = "./id_rsa.pub"
-}
-
-resource "proxmox_virtual_environment_file" "cloud_config" {
-  content_type = "snippets"
-  datastore_id = "snips"
-  node_name    = "kube"
-
-  source_raw {
-    data = <<EOF
-#cloud-config
-runcmd:
-    - apt update
-    # - apt install -y qemu-guest-agent net-tools
-    # - timedatectl set-timezone America/Toronto
-    # - systemctl enable qemu-guest-agent
-    # - systemctl start qemu-guest-agent
-    # - echo "done" > /tmp/vendor-cloud-init-done
-    EOF
-
-    file_name = "cloud-config.yaml"
-  }
-}
-
 resource "proxmox_virtual_environment_vm" "ubuntu_vm_cloned" {
-  depends_on = [proxmox_virtual_environment_vm.ubuntu_vm]
-  name       = "ubuntu-2204-clone"
+  name       = "ubuntu-test"
   node_name  = "kube"
-  vm_id      = 100
+  vm_id      = 1000
 
   clone {
     vm_id = 100
   }
 
+  cpu {
+    cores = 2
+  }
+  memory {
+    dedicated = 8000
+  }
+
   initialization {
-    #    interface = "scsi0"
     ip_config {
       ipv4 {
         address = "dhcp"
       }
     }
-
-    user_account {
-      username = "ubuntu"
-      keys     = [trimspace(data.local_file.ssh_public_key.content)]
-    }
-
-  
   }
 
   started = true
